27c74d9a13797caed04f891326d937151c16e05f
unknown <sam.sh0205@gmail.com>
2022-03-04 14:00:02 +0530

    first comm

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+
+http.listen(3000);
+
+app.set('view engine', 'ejs');
+app.get('/', (req, res)=>{
+    //res.send('Congo..the app works!!');
+    res.render('index', { time : new Date().toLocaleString()});
+})
+
+app.get('/about', (req, res)=>{
+    res.render('index', { time : new Date().toLocaleTimeString()});
+})
